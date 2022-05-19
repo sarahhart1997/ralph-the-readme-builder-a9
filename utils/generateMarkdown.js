787aeaa0644bc459de2aee8dddadf804a910f3af
Sarah Hart <sarah.hart.nc@gmail.com>
2022-05-19 16:26:27 -0400

    update license

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -40,7 +40,9 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return `This project is licensed under ${renderLicenseLink(license.licenseName)}.`
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data,license) {
