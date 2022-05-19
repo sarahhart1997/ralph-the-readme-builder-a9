3f11eca712d60f13f9eb701086490228d46aef1e
Sarah Hart <sarah.hart.nc@gmail.com>
2022-05-19 16:06:32 -0400

    updates

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -20,7 +20,18 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === '') {
+    return ``
+  }
+  if (license === 'MIT License') {
+    return `[MIT]()`
+  }
+  if (license === 'GPL') {
+    return `[GPL]()`
+  }
+  if (lic)
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
